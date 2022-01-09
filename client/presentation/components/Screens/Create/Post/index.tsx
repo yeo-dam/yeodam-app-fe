74ebf2a72c256bd84de996b9a0431cef4b15f33d
joonho lee <goodfluence@naver.com>
2022-01-10 00:02:14 +0900

    YA-133 done

--- client/presentation/components/Screens/Create/Post/index.tsx
+++ client/presentation/components/Screens/Create/Post/index.tsx
@@ -6,37 +6,84 @@ import Typography from "~presentation/components/Shared/Typography";
 import styled from "styled-components/native";
 import { RootTabScreenProps } from "types";
 import { WithLocalSvg } from "react-native-svg";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import ImageUpload from "~presentation/components/Shared/ImageUpload";
 import { useForm } from "react-hook-form";
 import Form from "~presentation/components/Shared/Form";
 import classValidatorResolver from "~domain/helper/classValidator";
 import PostModel from "~domain/model/PostModel/model";
+import Input from "~presentation/components/Shared/Input";
 
 const CreatePost = ({ navigation }: RootTabScreenProps<"CreatePost">) => {
+  const [isFront, setIsFront] = React.useState(false);
   return (
     <ContentLayout>
-      <Wrapper>
-        <Form<PostModel> schema={PostModel}>
-          <ImageUpload name="Image">
-            <ImageUploadSection>
-              <WithLocalSvg
-                asset={require("~asset/images/No_image.svg")}
-              ></WithLocalSvg>
-              <ImageUploadText>이미지를 넣어주세요!</ImageUploadText>
-            </ImageUploadSection>
-          </ImageUpload>
-        </Form>
-      </Wrapper>
+      <Form<PostModel> schema={PostModel}>
+        <Wrapper isFront={isFront}>
+          {isFront ? (
+            <ImageUpload name="Image">
+              <ImageUploadSection>
+                <WithLocalSvg
+                  asset={require("~asset/images/No_image.svg")}
+                ></WithLocalSvg>
+                <ImageUploadText>이미지를 넣어주세요!</ImageUploadText>
+              </ImageUploadSection>
+            </ImageUpload>
+          ) : (
+            <View>
+              <Input
+                name="user"
+                placeholder="user"
+                placeholderTextColor="#fff"
+              />
+              <Input
+                name="place"
+                placeholder="place"
+                placeholderTextColor="#fff"
+              />
+              <Input
+                name="title"
+                placeholder="title"
+                placeholderTextColor="#fff"
+              />
+              <Input
+                name="description"
+                placeholder="description"
+                placeholderTextColor="#fff"
+              />
+              <Input
+                name="createdAt"
+                placeholder="createdAt"
+                placeholderTextColor="#fff"
+              />
+              <Input
+                name="updatedAt"
+                placeholder="updatedAt"
+                placeholderTextColor="#fff"
+              />
+              <Input
+                name="images"
+                placeholder="images"
+                placeholderTextColor="#fff"
+              />
+              <Input
+                name="comments"
+                placeholder="comments"
+                placeholderTextColor="#fff"
+              />
+            </View>
+          )}
+        </Wrapper>
+      </Form>
     </ContentLayout>
   );
 };
 
 export default observer(CreatePost);
 
-const Wrapper = styled.View`
+const Wrapper = styled.View<{ isFront?: boolean }>`
   margin: 0 auto;
-  background-color: #fff;
+  background-color: ${({ isFront }) => (isFront ? "#fff" : "#121212")};
   width: 351px;
   height: 526px;
 `;
--- client/presentation/components/Shared/Input/index.tsx
+++ client/presentation/components/Shared/Input/index.tsx
@@ -1,27 +1,30 @@
 import React, { FC } from "react";
 import { Controller, useFormContext } from "react-hook-form";
 import { TextInput, TextInputProps } from "react-native";
+import styled from "styled-components/native";
 
 export type Props = {
   name: string;
 } & TextInputProps;
 
-const Component: FC<Props> = ({ name }) => {
+const Component: FC<Props> = ({ name, ...rest }) => {
   const { control } = useFormContext();
 
   return (
     <Controller
       name={name}
       control={control}
-      render={({ field }) => (
-        <TextInput
-          {...field}
-          onChange={() => console.log("onChange")}
-          value={"value"}
-        />
+      render={({ field: { onChange, value } }) => (
+        <StyledTextInput {...rest} onChange={onChange} value={value} />
       )}
     />
   );
 };
 
 export default Component;
+
+const StyledTextInput = styled.TextInput`
+  width: 100px;
+  height: 50px;
+  color: white;
+`;
