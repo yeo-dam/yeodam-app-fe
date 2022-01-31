e4a569de74808e2433fb28ddf073c9482519e911
joonho lee <goodfluence@naver.com>
2022-01-31 16:40:58 +0900

    YA-157 done

--- client/presentation/components/Local/GoogleMap/index.tsx
+++ client/presentation/components/Local/GoogleMap/index.tsx
@@ -1,29 +1,43 @@
 import * as React from "react";
+import { useEffect, useState } from "react";
 import MapView, {
   MapViewProps,
   Marker,
   PROVIDER_GOOGLE,
+  Region,
 } from "react-native-maps";
 import { Dimensions } from "react-native";
 import styled from "styled-components/native";
+import * as Location from "expo-location";
 import { observer } from "mobx-react";
+import useIsMounted from "hooks/useIsMounted";
+import MapViewModel from "~presentation/components/Screens/MyPage/Map/Map.vm";
 
 type Props = {
+  vm: MapViewModel;
   latitude?: number;
   longitude?: number;
 } & MapViewProps;
 
-const GoogleMap = ({ latitude = 37.5326, longitude = 127.024612 }: Props) => {
-  console.log(
-    `TCL ~ [index.tsx] ~ line ~ 13 ~ latitude
-longitude`,
-    latitude,
-    longitude
-  );
+const GoogleMap = ({
+  vm,
+  latitude = 37.5326,
+  longitude = 127.024612,
+  region,
+  onRegionChange,
+}: Props) => {
+  useEffect(() => {
+    async function loadPlaces() {
+      await vm.load();
+    }
+    loadPlaces();
+    return () => console.log("cleanup");
+  }, []);
 
   return (
     <Wrapper>
       <StyledMapView
+        region={region}
         provider={PROVIDER_GOOGLE}
         initialRegion={{
           latitude,
@@ -31,12 +45,21 @@ longitude`,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
-      <Marker
-        coordinate={{ latitude, longitude }}
-        title="this is a marker"
-        description="this is a marker example"
-      />
+        onRegionChange={onRegionChange}
+      >
+        {vm.places.map((item) => {
+          return (
+            <Marker
+              coordinate={{
+                latitude: item.latitude,
+                longitude: item.longitude,
+              }}
+              title={item.title}
+              description={item.description}
+            />
+          );
+        })}
+      </StyledMapView>
     </Wrapper>
   );
 };
--- client/presentation/components/Screens/MyPage/Map/index.tsx
+++ client/presentation/components/Screens/MyPage/Map/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
-import * as Location from "expo-location";
 
 import ContentLayout from "~presentation/components/Layout/ContentLayout";
 import { View } from "~presentation/components/Themed";
@@ -9,48 +8,37 @@ import ErrorMsg from "~presentation/components/Shared/ErrorMsg";
 import Loadable from "~presentation/components/Shared/Loadable";
 import { getRootViewModel } from "../../Index.vm";
 import MapViewModel from "./Map.vm";
-import { observer } from "mobx-react";
 import { MAIN_SCREEN_NAME } from "../../Main";
 import GoogleMap from "~presentation/components/Local/GoogleMap";
+import * as Location from "expo-location";
+import useIsMounted from "hooks/useIsMounted";
+import { Region } from "react-native-maps";
 
 const Map = ({
   navigation,
 }: RootTabScreenProps<typeof MAIN_SCREEN_NAME.MAP>) => {
   const vm = getRootViewModel<MapViewModel>((viewModel) => viewModel.tab.Map);
-  const [location, setLocation] = useState<{
-    latitude: number;
-    longitude: number;
-  }>();
+  const [location, setLocation] = useState<Region>();
+  const isMounted = useIsMounted();
 
-  // TODO : 현재 위치 가져오는 단계에서 에러 발생
   useEffect(() => {
-    // async function loadPosts() {
-    //   await vm.load();
-    // }
-    // loadPosts();
-
-    Location.installWebGeolocationPolyfill();
-    const { geolocation } = navigator;
-
-    if (!geolocation) {
-      throw Error("Geolocation is not supported.");
-    }
-
-    geolocation.getCurrentPosition(
-      (position) => {
-        console.log(`TCL ~ [index.tsx] ~ line ~ 41 ~ position`, position);
-        const { latitude, longitude } = position.coords;
-        setLocation({
-          latitude,
-          longitude,
-        });
-      },
-      (error) => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
         vm.setError(true);
-        console.error(error.message);
+        return;
       }
-    );
-  }, []);
+
+      let locationInfo = await Location.getCurrentPositionAsync({});
+
+      setLocation({
+        latitude: locationInfo.coords.latitude,
+        longitude: locationInfo.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    })();
+  }, [isMounted]);
 
   if (vm.isLoading) {
     return <Loadable />;
@@ -64,13 +52,18 @@ const Map = ({
     <ContentLayout title="Tab Three">
       <View>
         <GoogleMap
+          vm={vm}
+          region={location}
           latitude={location?.latitude}
           longitude={location?.longitude}
-          onRegionChange={() => setLocation(location)}
+          onRegionChange={(region) => {
+            // console.log(`TCL ~ [index.tsx] ~ line ~ 60 ~ region`, region);
+            // setLocation(region);
+          }}
         />
       </View>
     </ContentLayout>
   );
 };
 
-export default observer(Map);
+export default Map;
